fc7df9e7d681dd8299a8448c3e176c757a6ff311
smyilik <85386771+smyilik@users.noreply.github.com>
2021-12-16 21:46:18 +0300

    Update EL_09_itc_super_shift_list.cpp

--- EL_09_itc_super_shift_list.cpp
+++ EL_09_itc_super_shift_list.cpp
@@ -3,7 +3,7 @@
 void itc_super_shift_list(vector <int>& mass, int n)
 {
 	if (mass.size() <= 1) return;
-	if (n < 0) n = mass.size() - 1 + (n % mass.size());
+	if (n < 0) n = mass.size() - ((n * -1) % mass.size());
 	n %= mass.size();
 	if (n == 0) return;
 	for (int i = 0; i < n; i++) itc_rshift_list(mass);
